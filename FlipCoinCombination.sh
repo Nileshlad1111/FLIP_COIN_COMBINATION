354599e7dfe8fe5f2e48448a41c54c0a7beb2816
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-08 16:31:51 +0530

    Added:To check the head and tail

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,2 +1,14 @@
 #!/bin/bash -x
 echo "-------------------------------------------------------------------------FLIP COIN COMBINATION----------------------------------------------------------------------------------------"
+
+#CONSTANT
+HEAD=0
+
+#TO PRINT THE HEAD AND TAIL
+flipCoin=$((RANDOM%2))
+if [ $flipCoin == $HEAD ]
+then
+	echo "Head"
+else
+	echo "Tail"
+fi
