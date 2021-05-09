8df961ef5658cb43b086d3412b3c4c2403402373
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-05-09 21:55:43 +0530

    Added:To check the head and tail singlet print and percentage check

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,14 +1,36 @@
-#!/bin/bash -x
-echo "-------------------------------------------------------------------------FLIP COIN COMBINATION----------------------------------------------------------------------------------------"
+#!/bin/bash 
 
 #CONSTANT
 HEAD=0
 
-#TO PRINT THE HEAD AND TAIL
-flipCoin=$((RANDOM%2))
-if [ $flipCoin == $HEAD ]
-then
-	echo "Head"
-else
-	echo "Tail"
-fi
+#VARIABLES
+headCount=0
+tailCount=0
+
+#TO DECLARE DICTIONARY
+declare -A singletFlip
+
+#USER INPUT 
+read -p "Enter the Number of Coin Flip : " numberOfCoinFlip
+
+#TO STORE HEAD COUNT AND TAIL COUNT IN DICTIONARY
+for(( count=0; count<$numberOfCoinFlip; count++ ))
+do
+   FlipCoin=$(( RANDOM % 2 ))
+
+   if [ $FlipCoin -eq $HEAD ]
+   then
+      singletFlip[HEAD]=$((++headCount))
+   else
+      singletFlip[TAIL]=$((++tailCount))
+   fi
+done
+
+#TO PERCENTAGE
+singletHeadPercentage=$(($headCount * 100 / $numberOfCoinFlip))
+singletTailPercentage=$(($tailCount *100 / $numberOfCoinFlip))
+
+#TO PRINT
+echo "To single head percentage and head count:$headCount : " $singletHeadPercentage
+echo "To single tail percentage and tail count:$tailCount : " $singletTailPercentage
+
